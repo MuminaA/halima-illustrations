dbcafb459bec04c9a9e49d39db6c793756b764be
mina <muminaabdi13@gmail.com>
2024-12-03 23:14:17 -0500

    first commit, envornment setup

---
+++ backend/src/api/sub-categorie/controllers/sub-categorie.ts
@@ -0,0 +1,7 @@
+/**
+ * sub-categorie controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sub-categorie.sub-categorie');
---
+++ backend/src/api/sub-categorie/routes/sub-categorie.ts
@@ -0,0 +1,7 @@
+/**
+ * sub-categorie router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::sub-categorie.sub-categorie');
